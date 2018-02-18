40ff14a067283132d59727a82ca4e7e067fe59bb
Anton Firszov <antonfir@gmail.com>
2018-02-18 18:05:24 +0100

    console app

---
+++ VectorEqualityConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Numerics;
+
+namespace VectorEqualityConsole
+{
+    public struct Vector2Wrapper
+    {
+        public Vector2 BackingVector;
+
+        public Vector2Wrapper(float x, float y)
+        {
+            this.BackingVector = new Vector2(x, y);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector2Wrapper a = new Vector2Wrapper(0.22f, 0.61f);
+            Vector2Wrapper b = new Vector2Wrapper(0.22f, 0.61f);
+
+            bool equality = a.BackingVector.Equals(b.BackingVector);
+
+            Console.WriteLine(Environment.Version);
+            Console.WriteLine("Expected: True | Actual: "+equality);
+            Console.ReadLine();
+        }
+    }
+}
