8c434d470bba40c60aa42104159bbfd4c2401757
Anton Firszov <antonfir@gmail.com>
2018-02-19 18:12:54 +0100

    adding Vector3 and Vector4 tests

--- VectorEqualityBug/VectorEqualityTests.cs
+++ VectorEqualityBug/VectorEqualityTests.cs
@@ -15,6 +15,26 @@ namespace VectorEqualityBug
         }
     }
 
+    public struct Vector4Wrapper
+    {
+        public Vector4 BackingVector;
+
+        public Vector4Wrapper(float x, float y, float z, float w)
+        {
+            this.BackingVector = new Vector4(x, y, z, w);
+        }
+    }
+
+    public struct Vector3Wrapper
+    {
+        public Vector3 BackingVector;
+
+        public Vector3Wrapper(float x, float y, float z)
+        {
+            this.BackingVector = new Vector3(x, y, z);
+        }
+    }
+
     public class NestedVectorEqualityMagic
     {
         [Fact]
@@ -42,6 +62,24 @@ namespace VectorEqualityBug
             Vector2Wrapper b = new Vector2Wrapper(0.22f, 0.61f);
 
             Assert.True(a.BackingVector.Equals((object)b.BackingVector));
+        }
+
+        [Fact]
+        public void Vector4Test()
+        {
+            Vector4Wrapper a = new Vector4Wrapper(0.22f, 0.61f, 88.8f, 123);
+            Vector4Wrapper b = new Vector4Wrapper(0.22f, 0.61f, 88.8f, 123);
+
+            Assert.True(a.BackingVector.Equals(b.BackingVector));
+        }
+
+        [Fact]
+        public void Vector3Test()
+        {
+            Vector3Wrapper a = new Vector3Wrapper(0.22f, 0.61f, 88.8f);
+            Vector3Wrapper b = new Vector3Wrapper(0.22f, 0.61f, 88.8f);
+
+            Assert.True(a.BackingVector.Equals(b.BackingVector));
         }
     }
 }
